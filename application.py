e47da7a38b39643a14fae3f1a3f51277cc57c905
StephenTrainor <stephontrainor@gmail.com>
2020-12-16 23:00:51 -0800

    Formatting

--- application.py
+++ application.py
@@ -25,13 +25,11 @@ def after_request(response):
     return response
     
 
-# Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///data.db")
 
 
@@ -146,55 +144,39 @@ def manager():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """Log user in"""
-
-    # Forget any user_id
     session.clear()
 
-    # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
 
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
-        # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
-        # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username",
                           username=request.form.get("username"))
 
-        # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
-        # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
 
-        # Redirect user to home page
         return redirect("/")
 
-    # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
-    """Log user out"""
-
-    # Forget any user_id
     session.clear()
 
-    # Redirect user to login form
     return redirect("/")
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    """Register user"""
 
     session.clear()
 
@@ -204,15 +186,12 @@ def register():
     if request.method == "POST":
         user_password = request.form.get("password")
 
-        # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
 
-        # Ensure password was submitted
         elif not user_password:
             return apology("must provide password", 403)
 
-        # Ensure password and confirmed password match before continue
         elif user_password != request.form.get("confirm-password"):
             return apology("Password and Confirmed Password did not match", 403)
 
@@ -220,7 +199,6 @@ def register():
             if c == " ":
                 return apology("Password cannot have spaces in them.", 403)
 
-        # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = :username", username=request.form.get("username"))
 
         if len(rows) > 0:
@@ -253,12 +231,10 @@ def register():
 
 
 def errorhandler(e):
-    """Handle error"""
     if not isinstance(e, HTTPException):
         e = InternalServerError()
     return apology(e.name, e.code)
 
 
-# Listen for errors
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
